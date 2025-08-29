16cd63a9bc69641e19c56d0b13418f6bd6f4b7fc
Xlence1624 <xlence1624@gmail.com>
2025-08-29 14:32:39 +0100

    changes
    Signed-off-by: Xlence1624 <xlence1624@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Welcome from './components/pages/Welcome.vue';
 import Layouts from './components/layouts/Layouts.vue'; 
 import Dashboard from './components/pages/Dashboard.vue';
 import Workout from './components/pages/Workout.vue';
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { workoutProgram } from './utils';
 const selectedDisplay = ref(1) 
 
@@ -48,6 +48,17 @@ function handleSelectedPlan (){
   data.value = defaultData
   localStorage.removeItem('workouts')
 }
+
+onMounted(
+  () => {
+if(!localStorage){return}
+if(localStorage.getItem('workouts')){
+  const savedData = JSON.parse(localStorage.getItem('workouts'))
+  data.value = savedData
+  selectedDisplay.value = 2
+}
+  }
+)
 
 const isWorkoutComplete = computed(
 () => 
